3d9570124c299336311229eb9f7dd39b269b9d78
streamich <streamich@gmail.com>
2024-07-24 18:40:15 +0200

    feat: 🎸 make undo-redo stack async

--- src/json-crdt-repo/UndoRedoStack.ts
+++ src/json-crdt-repo/UndoRedoStack.ts
@@ -1,9 +1,14 @@
+import {timeout} from 'thingies/lib/timeout';
+
+const timeout2 = <T>(ms: number, promise: Promise<T>): Promise<T | undefined> =>
+    timeout(ms, promise).catch(() => undefined);
+
 export interface UndoItem {
-  undo(): RedoItem;
+  undo(): RedoItem | Promise<RedoItem>;
 }
 
 export interface RedoItem {
-  redo(): UndoItem;
+  redo(): UndoItem | Promise<UndoItem>;
 }
 
 export class UndoRedoStack {
@@ -25,17 +30,43 @@ export class UndoRedoStack {
     return redoStack;
   }
 
-  public undo(): void {
-    const undo = this.undoStack.pop();
-    if (!undo) return;
-    const redo = undo.undo();
-    this.redoStack.push(redo);
+  private locked = false;
+
+  public async undo(): Promise<-2 | -1 | 0 | 1> {
+    if (this.locked) return 0;
+    this.locked = true;
+    try {
+      const undo = this.undoStack.pop();
+      if (!undo) return -1;
+      let redo: RedoItem | Promise<RedoItem> | undefined = undo.undo();
+      if (redo && typeof redo === 'object' && typeof (redo as any).then === 'function') {
+        redo = await timeout2(2000, Promise.resolve(redo));
+        if (!redo) return -2;
+      }
+      this.redoStack.push(redo as RedoItem);
+      return 1;
+    } finally {
+      this.locked = false;
+    }
   }
 
-  public redo(): void {
-    const redo = this.redoStack.pop();
-    if (!redo) return;
-    const undo = redo.redo();
-    this.undoStack.push(undo);
+  public async redo(): Promise<-3 | -2 | -1 | 0 | 1> {
+    if (this.locked) return 0;
+    this.locked = true;
+    try {
+      const redo = this.redoStack.pop();
+      if (!redo) return -1;
+      let undo: UndoItem | Promise<UndoItem> | undefined = redo.redo();
+      if (undo && typeof undo === 'object' && typeof (undo as any).then === 'function') {
+        undo = await timeout2(2000, Promise.resolve(undo));
+        if (!undo) return -2;
+      }
+      this.undoStack.push(undo as UndoItem);
+      return 1;
+    } catch (error) {
+      return -3;
+    } finally {
+      this.locked = false;
+    }
   }
 }
