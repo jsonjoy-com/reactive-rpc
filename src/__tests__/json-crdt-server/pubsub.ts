9cdcefdfc029d2a6bb54cefd64f30706661fbb2c
streamich <streamich@gmail.com>
2024-08-01 23:32:58 +0200

    test: 💍 harden naming in pubsub tests

--- src/__tests__/json-crdt-server/pubsub.ts
+++ src/__tests__/json-crdt-server/pubsub.ts
@@ -2,6 +2,8 @@ import {tick, until} from 'thingies';
 import type {ApiTestSetup} from '../../common/rpc/__tests__/runApiTests';
 import type {JsonCrdtTestSetup} from '../../__demos__/json-crdt-server/__tests__/setup';
 
+const channelId = () => 'channel-' + Math.random().toString(36).slice(2) + '-' + Date.now().toString(36);
+
 export const runPubsubTests = (_setup: ApiTestSetup, params: {staticOnly?: true} = {}) => {
   const setup = _setup as JsonCrdtTestSetup;
 
@@ -10,7 +12,7 @@ export const runPubsubTests = (_setup: ApiTestSetup, params: {staticOnly?: true}
       const {call, stop} = await setup();
       try {
         await call('pubsub.publish', {
-          channel2: 'INVALID KEY',
+          channel2: channelId(),
           message: 'hello world',
         } as any);
         throw new Error('should not reach here');
@@ -24,11 +26,12 @@ export const runPubsubTests = (_setup: ApiTestSetup, params: {staticOnly?: true}
       test('can subscribe and receive published messages', async () => {
         const {call, call$, stop} = await setup();
         const emits: any[] = [];
-        const subscription = call$('pubsub.listen', {channel: 'my-channel'}).subscribe((res) => {
+        const channel = channelId();
+        const subscription = call$('pubsub.listen', {channel}).subscribe((res) => {
           emits.push(res.message);
         });
         await call('pubsub.publish', {
-          channel: 'my-channel',
+          channel,
           message: 'hello world',
         });
         await until(() => emits.length === 1);
@@ -40,22 +43,23 @@ export const runPubsubTests = (_setup: ApiTestSetup, params: {staticOnly?: true}
       test('does not receive messages after un-subscription', async () => {
         const {call, call$, stop} = await setup();
         const emits: any[] = [];
-        const sub = call$('pubsub.listen', {channel: 'my-channel'}).subscribe((res) => {
+        const channel = channelId();
+        const sub = call$('pubsub.listen', {channel}).subscribe((res) => {
           emits.push(res.message);
         });
         await call('pubsub.publish', {
-          channel: 'my-channel',
+          channel,
           message: 'msg1',
         });
         await call('pubsub.publish', {
-          channel: 'my-channel',
+          channel,
           message: 'msg2',
         });
         await until(() => emits.length === 2);
         sub.unsubscribe();
         await tick(25);
         await call('pubsub.publish', {
-          channel: 'my-channel',
+          channel,
           message: 'msg3',
         });
         await tick(50);
@@ -66,23 +70,26 @@ export const runPubsubTests = (_setup: ApiTestSetup, params: {staticOnly?: true}
 
       test('multiple multiple subscribers can subscribe to multiple channels', async () => {
         const {call, call$, stop} = await setup();
+        const channel1 = channelId();
+        const channel2 = channelId();
+        const channel3 = channelId();
         const user1: any[] = [];
         const user2: any[] = [];
         const user3: any[] = [];
-        call$('pubsub.listen', {channel: 'channel-1'}).subscribe((res) => {
+        call$('pubsub.listen', {channel: channel1}).subscribe((res) => {
           user1.push(res.message);
         });
-        const sub2 = call$('pubsub.listen', {channel: 'channel-2'}).subscribe((res) => {
+        const sub2 = call$('pubsub.listen', {channel: channel2}).subscribe((res) => {
           user2.push(res.message);
         });
-        call$('pubsub.listen', {channel: 'channel-1'}).subscribe((res) => {
+        call$('pubsub.listen', {channel: channel1}).subscribe((res) => {
           user3.push(res.message);
         });
-        call$('pubsub.listen', {channel: 'channel-2'}).subscribe((res) => {
+        call$('pubsub.listen', {channel: channel2}).subscribe((res) => {
           user3.push(res.message);
         });
         await call('pubsub.publish', {
-          channel: 'my-channel',
+          channel: channel3,
           message: 'hello world',
         });
         await tick(50);
@@ -90,11 +97,11 @@ export const runPubsubTests = (_setup: ApiTestSetup, params: {staticOnly?: true}
         expect(user2).toStrictEqual([]);
         expect(user3).toStrictEqual([]);
         call('pubsub.publish', {
-          channel: 'channel-1',
+          channel: channel1,
           message: 'msg1',
         }).catch(() => {});
         call('pubsub.publish', {
-          channel: 'channel-2',
+          channel: channel2,
           message: 'msg2',
         }).catch(() => {});
         await until(() => user1.length === 1);
@@ -106,7 +113,7 @@ export const runPubsubTests = (_setup: ApiTestSetup, params: {staticOnly?: true}
         expect(user3.indexOf('msg2') > -1).toBe(true);
         sub2.unsubscribe();
         call('pubsub.publish', {
-          channel: 'channel-2',
+          channel: channel2,
           message: 'msg3',
         }).catch(() => {});
         await until(() => user3.length === 3);
