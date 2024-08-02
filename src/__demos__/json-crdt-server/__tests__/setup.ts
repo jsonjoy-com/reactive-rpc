a17d6999faed13b5884d50cb00eb384a92650369
streamich <streamich@gmail.com>
2024-08-02 23:19:20 +0200

    test: 💍 run different combinations of tests

--- src/__demos__/json-crdt-server/__tests__/setup.ts
+++ src/__demos__/json-crdt-server/__tests__/setup.ts
@@ -4,16 +4,10 @@ import {createCaller} from '../routes';
 import {LevelStore} from '../services/blocks/store/level/LevelStore';
 import {Services} from '../services/Services';
 import {ClassicLevel} from 'classic-level';
+import {MemoryStore} from '../services/blocks/store/MemoryStore';
+import type {Store} from '../services/blocks/store/types';
 
-
-export const setup = async () => {
-  const kv = new ClassicLevel<string, Uint8Array>('./db', { valueEncoding: 'view' })
-  const open = kv.open();
-  // const kv = new MemoryLevel<string, Uint8Array>({
-  //   keyEncoding: 'utf8',
-  //   valueEncoding: 'view',
-  // });
-  const store = new LevelStore(<any>kv);
+export const setup = async (store: Store = new MemoryStore(), close?: () => Promise<void>) => {
   const services = new Services(store);
   const {caller} = createCaller(services);
   const {client} = buildE2eClient(caller, {
@@ -25,10 +19,31 @@ export const setup = async () => {
   });
   const call = client.call.bind(client);
   const call$ = client.call$.bind(client);
-  const stop = async () => {
-    await kv.close();
+  const stop = async (): Promise<void> => {
+    await close?.();
   };
   return {call, call$, stop};
+};
+
+export const setupMemory = async () => {
+  const store = new MemoryStore();
+  return setup(store);
+};
+
+export const setupLevelMemory = async () => {
+  const kv = new MemoryLevel<string, Uint8Array>({
+    keyEncoding: 'utf8',
+    valueEncoding: 'view',
+  });
+  const store = new LevelStore(<any>kv);
+  return setup(store);
+};
+
+export const setupLevelClassic = async () => {
+  const kv = new ClassicLevel<string, Uint8Array>('./db', { valueEncoding: 'view' })
+  await kv.open();
+  const store = new LevelStore(<any>kv);
+  return setup(store, async () => kv.close());
 };
 
 export type JsonCrdtTestSetup = typeof setup;
