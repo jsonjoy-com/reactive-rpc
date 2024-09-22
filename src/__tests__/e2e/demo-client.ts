90cc182a6a565d553577c24a7d803ec157ef5c9d
streamich <streamich@gmail.com>
2024-09-22 20:43:19 +0200

    test: 💍 add demo server test url

--- src/__tests__/e2e/demo-client.ts
+++ src/__tests__/e2e/demo-client.ts
@@ -3,10 +3,12 @@ import {RpcCodec} from '../../common/codec/RpcCodec';
 import {RpcPersistentClient, StreamingRpcClient, WebSocketChannel} from '../../common';
 import {FetchRpcClient} from '../../common/rpc/client/FetchRpcClient';
 
-const host = '54.204.106.133';
+const secure = true;
+const host = 'demo-iasd8921ondk0.jsonjoy.com';
+// const host = '127.0.0.1:8080';
 
 export const setupDemoServerPersistentClient = (codec: RpcCodec) => {
-  const url = `ws://${host}/rpc`;
+  const url = `ws${secure ? 's' : ''}://${host}/rpc`;
   const client = new RpcPersistentClient({
     codec,
     channel: {
@@ -24,7 +26,7 @@ export const setupDemoServerPersistentClient = (codec: RpcCodec) => {
 };
 
 export const setupDemoServerFetchClient = (codec: RpcCodec) => {
-  const url = `http://${host}/rpc`;
+  const url = `http${secure ? 's' : ''}://${host}/rpc`;
   const client = new FetchRpcClient({
     url,
     msgCodec: codec.msg,
@@ -43,7 +45,7 @@ export const setupDemoServerStreamingClient = (codec: RpcCodec) => {
   const client = new StreamingRpcClient({
     send: async (messages) => {
       const port = +(process.env.PORT || 9999);
-      const url = `http://${host}/rpc`;
+      const url = `http${secure ? 's' : ''}://${host}/rpc`;
       codec.req.encoder.writer.reset();
       codec.msg.encodeBatch(codec.req, messages);
       const body = codec.req.encoder.writer.flush();
